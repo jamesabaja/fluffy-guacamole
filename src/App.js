2c051c365708085be9955aabbdb0ff7e1a44e042
jgabaja <jgabaja@gmail.com>
2019-06-24 23:37:45 +0800

    add login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Login from './components/Login/Login';
@@ -17,9 +17,14 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/clinics" component={Dashboard} />
-        <Route exact path="/clinics/inventory" component={Inventory} />
-        <Route exact path="/clinics/order" component={Order} />
+        {localStorage.getItem('isAuthenticated') === 'true' ?
+        <div>
+          <Route exact path="/clinics" component={Dashboard} />
+          <Route exact path="/clinics/inventory" component={Inventory} />
+          <Route exact path="/clinics/order" component={Order} />
+        </div>
+        :
+        <Redirect to='/login' />}
         <Route exact path="/office" component={OfficeDashboard} />
         <Route exact path="/office/view/orders" component={ViewOrders} />
         {/* <Route exact path="/office/view/health_centers" component={ViewOrders} /> */}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Container, Icon, Input, Card, Button} from 'semantic-ui-react';
 import MenuBar from '../MenuBar/MenuBar';
+import axios from 'axios';
 
-const Login = () => (
-  <div>
-    <MenuBar />
-    <Container textAlign="center" className="login">
-      <Icon name="pills" color="teal" circular inverted size="huge"/>
-      <h1>MEDikts</h1>
-      <h4>Medicine Inventory Keeping and Tracking System</h4>
-      <br/>
-      <Card centered>
-        <Card.Content>
-          <Input icon='users' iconPosition='left' placeholder='Username' />
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  componentWillMount() {
+    localStorage.setItem('token', null);
+    localStorage.setItem('isAuthenticated', null);
+  }
+
+  onChange = (event, data) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+ logIn = () => {
+    axios.post('https://medikts-backend.herokuapp.com/api/token/', this.state)
+    .then(response => {
+      localStorage.setItem('token', response.data.access);
+      localStorage.setItem('isAuthenticated', 'true');
+      setTimeout(() => {
+        console.log(localStorage.getItem('isAuthenticated'))
+        this.props.history.push('/clinics');
+      }, 1000);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <MenuBar />
+        <Container textAlign="center" className="login">
+          <Icon name="pills" color="teal" circular inverted size="huge"/>
+          <h1>MEDikts</h1>
+          <h4>Medicine Inventory Keeping and Tracking System</h4>
           <br/>
-          <br/> 
-          <Input icon='key' iconPosition='left' placeholder='Password' />   
-        </Card.Content> 
-        <Card.Content extra>
-          <Button href='/clinics' fluid color='green'>
-            Login
-          </Button>
-        </Card.Content>
-      </Card>
-    </Container>
-  </div>
-    
-);
+          <Card centered>
+            <Card.Content>
+              <Input icon='users' name='username' iconPosition='left' placeholder='Username' onChange={this.onChange}/>
+              <br/>
+              <br/> 
+              <Input type='password' name='password' icon='key' iconPosition='left' placeholder='Password' onChange={this.onChange} />   
+            </Card.Content> 
+            <Card.Content extra>
+              <Button onClick={this.logIn} fluid color='green'>
+                Login
+              </Button>
+            </Card.Content>
+          </Card>
+        </Container>
+      </div>
+    )
+  }
+}
 
 export default Login
